542bd3ca257bf580b9aa517c1930b4324003cea4
Davis Too <dtoo@hudumakenya.go.ke>
2020-12-10 16:59:56 +0300

    added validate api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -24,7 +24,7 @@ class ApiController extends Controller
         // echo $url . $q;
         $res = $client->get($url . $q);
         // echo $res->getStatusCode(); // 200
-        dd($res->getBody());
+        dd($res->getBody()->getContents());
     }
 
     public function civilsearch(Request $request)
